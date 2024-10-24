2c6f559ebf2517c0419b6deb7285ac5c4f4b84b0
Shashank Phatkure <shashankphatkure@gmail.com>
2024-10-24 09:42:58 +0530

    updated hero area

--- app/property/components/PropertyHero.jsx
+++ app/property/components/PropertyHero.jsx
@@ -3,31 +3,25 @@ import PropertyModal from "./PropertyModal";
 
 export default function PropertyHero() {
   return (
-    <div className="carousel-3e1 slide-v6h">
-      <PropertyModal />
-      <ol className="carousel-w4a">
-        <li />
-
-        <li />
-      </ol>
-      <div className="carousel-swj">
-        <div className="carousel-item-5qd act-tvr">
-          <picture>
-            <img
-              className="block-kof"
-              src="https://newprojectsonline.com/assets/uploads/banners/1719552676-ashar-merac-mulund-banner.webp"
-            />
-          </picture>
+    <div className="property-hero">
+      <div className="carousel-container">
+        <div className="carousel-item active">
+          <img
+            className="hero-image"
+            src="https://newprojectsonline.com/assets/uploads/banners/1719552676-ashar-merac-mulund-banner.webp"
+            alt="Ashar Merac Thane"
+          />
         </div>
-        <div className="carousel-item-5qd">
-          <picture>
-            <img
-              className="laz-oa9 block-kof"
-              src="https://newprojectsonline.com/assets/uploads/banners/1719552678-ashar-merac-mulund-banner2.webp"
-            />
-          </picture>
+        <div className="carousel-item">
+          <img
+            className="hero-image"
+            src="https://newprojectsonline.com/assets/uploads/banners/1719552678-ashar-merac-mulund-banner2.webp"
+            alt="Ashar Merac Thane"
+          />
         </div>
       </div>
+
+      <PropertyModal />
     </div>
   );
 }
