f8c9bc11c701f149364eeb00811bdffe3ed2f2b3
Shashank Phatkure <shashankphatkure@gmail.com>
2024-11-06 21:23:00 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import { supabase } from "@/utils/supabase";
 import HomeContent from "@/components/HomeContent";
 
-// This becomes the main page component (Server Component)
 export default async function HomePage() {
   // Fetch properties from Supabase
   const { data: propertiesData } = await supabase
--- app/property/page.js
+++ app/property/page.js
@@ -6,47 +6,34 @@ import PropertyHeader from "./components/PropertyHeader";
 import PropertyHero from "./components/PropertyHero";
 import PropertyLocation from "./components/PropertyLocation";
 import PropertyOverview from "./components/PropertyOverview";
-import PropertyPopup from "./components/PropertyPopup";
 import PropertyPrice from "./components/PropertyPrice";
 import PropertySitePlan from "./components/PropertySitePlan";
 import PropertySiteTour from "./components/PropertySiteTour";
 import Sidebar from "./components/sidebar";
 
-export default function Page() {
+export default function PropertyPage() {
   return (
     <div className="flex">
-      <div className="w-3/4">
+      <main className="w-3/4">
         <PropertyHeader />
         <PropertyHero />
+
         <div className="px-3">
           <PropertyOverview />
-        </div>
-        <div className="px-3">
           <PropertyPrice />
-        </div>
-        <div className="px-3">
           <PropertySitePlan />
-        </div>
-        <div className="px-3">
           <PropertyAmenities />
-        </div>
-        <div className="px-3">
           <PropertyGallery />
-        </div>
-        <div className="px-3">
           <PropertyLocation />
-        </div>
-        <div className="px-3">
           <PropertySiteTour />
         </div>
-        <div className="px-3">
-          <PropertyFAQ />
-        </div>
+
         <PropertyFooter />
-      </div>
-      <div className="w-1/4">
+      </main>
+
+      <aside className="w-1/4">
         <Sidebar />
-      </div>
+      </aside>
     </div>
   );
 }
