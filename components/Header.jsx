e277e53c06105b3f5c0708068bd155e69343b275
Shashank Phatkure <shashankphatkure@gmail.com>
2024-11-06 21:20:24 +0530

    updated header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,42 +1,87 @@
 import "./Header.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone, faBars } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function Header() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    return (
-        <header className="absolute z-10 left-0 top-10 w-full bg-transparent py-4">
-            <div className="container mx-auto px-4">
-                <div className="flex justify-between items-center">
-                    <div className="logo">
-                        <a href="https://newprojectsonline.com/">
-                            <img src="https://newprojectsonline.com/assets/main/img/logos/logo.png" width={255} height={50} alt="Logo" />
-                        </a>
-                    </div>
-                    <div className="lg:hidden">
-                        <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-white">
-                            <FontAwesomeIcon icon={faBars} className="text-2xl" />
-                        </button>
-                    </div>
-                    <nav className={`lg:block ${isMenuOpen ? 'block' : 'hidden'} absolute lg:relative top-full left-0 w-full lg:w-auto bg-white lg:bg-transparent`}>
-                        <ul className="flex flex-col lg:flex-row">
-                            <li><a href="https://newprojectsonline.com/" className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80">Home</a></li>
-                            <li><a href="#about" className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80">About Us</a></li>
-                            <li><a href="#services" className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80">Services</a></li>
-                            <li><a href="#contact" className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80">Contact</a></li>
-                            <li><a href="https://newprojectsonline.com/privacy-policy.html" className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80">Privacy Policy</a></li>
-                        </ul>
-                    </nav>
-                    <div className="hidden lg:block">
-                        <a href="tel:+91-9137458691" className="inline-flex items-center bg-[#f64d0d] text-white px-4 py-2 rounded">
-                            <FontAwesomeIcon icon={faPhone} className="mr-2" />
-                            <span>+91-9137458691</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="absolute z-10 left-0 top-10 w-full bg-transparent py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center">
+          <div className="logo">
+            <a href="https://newprojectsonline.com/">
+              <img src="/logo.png" width={50} height={50} alt="Logo" />
+            </a>
+          </div>
+          <div className="lg:hidden">
+            <button
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="text-white"
+            >
+              <FontAwesomeIcon icon={faBars} className="text-2xl" />
+            </button>
+          </div>
+          <nav
+            className={`lg:block ${
+              isMenuOpen ? "block" : "hidden"
+            } absolute lg:relative top-full left-0 w-full lg:w-auto bg-white lg:bg-transparent`}
+          >
+            <ul className="flex flex-col lg:flex-row">
+              <li>
+                <a
+                  href="https://newprojectsonline.com/"
+                  className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#about"
+                  className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80"
+                >
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#services"
+                  className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#contact"
+                  className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80"
+                >
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://newprojectsonline.com/privacy-policy.html"
+                  className="block py-2 lg:py-0 px-4 text-black lg:text-white font-bold hover:text-opacity-80"
+                >
+                  Privacy Policy
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="hidden lg:block">
+            <a
+              href="tel:+91-9137458691"
+              className="inline-flex items-center bg-[#f64d0d] text-white px-4 py-2 rounded"
+            >
+              <FontAwesomeIcon icon={faPhone} className="mr-2" />
+              <span>+91-9137458691</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
