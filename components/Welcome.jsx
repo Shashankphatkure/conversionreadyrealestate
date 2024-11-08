cab7d0686ce19771b4463669cea1c2492545b577
Shashank Phatkure <shashankphatkure@gmail.com>
2024-11-08 21:44:02 +0530

    updated building towers

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -4,7 +4,7 @@ export default function Welcome() {
       <div className="welcome-left w-full md:w-1/2 md:pr-4 mb-4 md:mb-0">
         <div className="welcome-image">
           <img
-            src="https://newprojectsonline.com/assets/main/img/properties/properties-6.webp"
+            src="/towers.jpg"
             alt="Luxurious bedroom interior"
             className="w-full h-auto object-cover"
           />
@@ -13,7 +13,7 @@ export default function Welcome() {
       <div className="welcome-right w-full md:w-1/2 md:pl-4">
         <div className="welcome-text">
           <h2 className="text-xl md:text-2xl font-bold mb-4 text-orange-500">
-            Welcome to NewProjectsOnline.Com
+            Welcome to Shree Ganesh Realty
           </h2>
           <p className="mb-2 font-bold uppercase text-sm md:text-base">
             WE ARE A FAST-GROWING MARKETING COMPANY IN THE REAL ESTATE INDUSTRY.
