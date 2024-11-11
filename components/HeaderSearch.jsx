5b2832fa5c72a45d3e27bd6f714692e508663e48
Shashank Phatkure <shashankphatkure@gmail.com>
2024-11-12 03:14:36 +0530

    updated header background

--- components/HeaderSearch.jsx
+++ components/HeaderSearch.jsx
@@ -1,60 +1,64 @@
 import "./HeaderSearch.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function HeaderSearch() {
-    return (
-        <div className="hb-25x">
-            <div className="search-oyt">
-                <div className="container-ssc">
-                    <div className="content-zed">
-                        <div className="row-etd content-nj1 text-i2x">
-                            <div className="col-8nj">
-                                <h2>Find Your Dream Home With Us</h2>
-                            </div>
-                            <div className="col-8nj">
-                                <div className="search-vvo">
-                                    <div className="search-ze4">
-                                        <form>
-                                            <div className="row-etd content-nj1 no-maj">
-                                                <div className="col-oxy col-x5q d-yrc" />
-                                                <div className="col-sgs col-pzo">
-                                                    <div className="auto-jpq">
-                                                        <input 
-                                                            type="text" 
-                                                            name="project-name" 
-                                                            id="sea-6t3" 
-                                                            placeholder="Search by: Project Name/ Property Name/ Location" 
-                                                            style={{margin: "0 !important"}} 
-                                                        />
-                                                        <div style={{
-                                                            width: "96%",
-                                                            position: "absolute",
-                                                            zIndex: "999",
-                                                            height: "140px",
-                                                            overflow: "auto",
-                                                            display: "none",
-                                                            marginLeft: "0%",
-                                                            borderRadius: "10px"
-                                                        }} />
-                                                        <div id="sug-71o" />
-                                                    </div>
-                                                </div>
-                                                <div className="col-gbd col-hz4">
-                                                    <button type="submit">
-                                                        <FontAwesomeIcon icon={faSearch} className="hygin search-48v" />
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
+  return (
+    <div className="hb-25x">
+      <div className="search-oyt">
+        <div className="container-ssc">
+          <div className="content-zed">
+            <div className="row-etd content-nj1 text-i2x">
+              <div>
+                <h2>Find Your Dream Home With Us</h2>
+              </div>
+              <div className="col-8nj">
+                <div className="search-vvo">
+                  <div className="search-ze4">
+                    <form>
+                      <div className="row-etd content-nj1 no-maj">
+                        <div className="col-oxy col-x5q d-yrc" />
+                        <div className="col-sgs col-pzo">
+                          <div className="auto-jpq">
+                            <input
+                              type="text"
+                              name="project-name"
+                              id="sea-6t3"
+                              placeholder="Search by: Project Name/ Property Name/ Location"
+                              style={{ margin: "0 !important" }}
+                            />
+                            <div
+                              style={{
+                                width: "96%",
+                                position: "absolute",
+                                zIndex: "999",
+                                height: "140px",
+                                overflow: "auto",
+                                display: "none",
+                                marginLeft: "0%",
+                                borderRadius: "10px",
+                              }}
+                            />
+                            <div id="sug-71o" />
+                          </div>
                         </div>
-                    </div>
+                        <div className="col-gbd col-hz4">
+                          <button type="submit">
+                            <FontAwesomeIcon
+                              icon={faSearch}
+                              className="hygin search-48v"
+                            />
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
