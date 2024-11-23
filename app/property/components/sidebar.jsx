ddb6f1f458e41503adc9b0f71c5b77b11efd1eac
Shashank Phatkure <shashankphatkure@gmail.com>
2024-11-23 20:04:18 +0530

    updated margin

--- app/property/components/sidebar.jsx
+++ app/property/components/sidebar.jsx
@@ -93,7 +93,7 @@ export default function Sidebar({ property }) {
       )}
 
       <form className="px-4" onSubmit={handleSubmit}>
-        <div className="mb-4">
+        <div className="mb-0">
           <input
             type="text"
             name="name"
